b96dc13ec7e4acddf7d4fc4f045dd640dd51fe82
Evgeniya Khatulkova <hatulkova.ev@gmail.com>
2022-12-20 13:19:49 +0100

    create functions for simple addition, substraction, multiplication and division

---
+++ script-calculator.js
@@ -0,0 +1,15 @@
+function addition(total, current) {
+    return total + current;
+}
+
+function substract(total,current) {
+    return total - current;
+}
+
+function multiple(total, current) {
+    return total * current;
+}
+
+function division(total, current) {
+    return total / current;
+}
