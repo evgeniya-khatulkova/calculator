7327e033ba204af12d4af312d09c59e3c8aa6286
Evgeniya Khatulkova <hatulkova.ev@gmail.com>
2022-12-20 22:40:27 +0100

    add operate function

--- script-calculator.js
+++ script-calculator.js
@@ -1,3 +1,8 @@
+const add = "addition";
+const sub = "substraction";
+const mult = "multiplication";
+const div = "division";
+
 function addition(total, current) {
     return total + current;
 }
@@ -12,4 +17,23 @@ function multiple(total, current) {
 
 function division(total, current) {
     return total / current;
+}
+
+function operate(total, operator, current) {
+    if (operator === "addition") {
+        let result = addition (total,current);
+        return result;
+    } 
+    else if (operator === "substraction") {
+        let result = substract (total,current);
+        return result;
+    }
+    else if (operator === "multiplication") {
+        let result = multiple(total,current);
+        return result;
+    }
+    else if(operator === "division") {
+        let result = division(total,current);
+        return result;
+    }
 }
