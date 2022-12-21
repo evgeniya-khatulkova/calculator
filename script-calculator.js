d8efcbccfcc92b04479b86e5c22e09038057c5f4
Evgeniya Khatulkova <hatulkova.ev@gmail.com>
2022-12-22 00:12:52 +0100

    create a function to populate a number on the display after pressing a number- button

--- script-calculator.js
+++ script-calculator.js
@@ -3,6 +3,22 @@ const sub = "substraction";
 const mult = "multiplication";
 const div = "division";
 
+var windowView = document.getElementById("window");
+// var buttons = document.getElementById("1");
+const buttons = document.querySelectorAll('button');
+
+//to populate number on the display after pressing the button
+for (i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function (e) {
+        windowView.textContent += e.target.id;
+    });
+}
+
+// buttons.addEventListener('click', function (e) {
+//     windowView.textContent += buttons.textContent;
+// });
+
+//simple functions to count
 function addition(total, current) {
     return total + current;
 }
@@ -19,6 +35,7 @@ function division(total, current) {
     return total / current;
 }
 
+//one function to operate 
 function operate(total, operator, current) {
     if (operator === "addition") {
         let result = addition (total,current);
