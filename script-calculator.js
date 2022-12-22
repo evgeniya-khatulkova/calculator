928aeea8125abdc911c5fd097cccb9dfbf9d325f
Evgeniya Khatulkova <hatulkova.ev@gmail.com>
2022-12-23 00:49:42 +0100

    store numbers from input and classify number-buttons

--- script-calculator.js
+++ script-calculator.js
@@ -2,17 +2,25 @@ const add = "addition";
 const sub = "substraction";
 const mult = "multiplication";
 const div = "division";
+const numberInUse = {
+    first: '',
+};
 
 var windowView = document.getElementById("window");
 // var buttons = document.getElementById("1");
-const buttons = document.querySelectorAll('button');
+const buttonsNum = document.querySelectorAll('.numbers');
 
+console.log(buttonsNum);
 //to populate number on the display after pressing the button
-for (i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function (e) {
+for (i = 0; i < buttonsNum.length; i++) {
+    buttonsNum[i].addEventListener('click', function (e) {
         windowView.textContent += e.target.id;
+        var toInt = [];
+        toInt = e.target.id;
+        numberInUse.first += toInt;
     });
 }
+
 
 // buttons.addEventListener('click', function (e) {
 //     windowView.textContent += buttons.textContent;
